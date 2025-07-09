0685974edba83278fa3e73b76c8ef9049284664a
Tarique Ansari <113173007+tarique143@users.noreply.github.com>
2025-07-09 15:24:18 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,19 @@
+# postgraysql
 # file: database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
 
-# Apne MySQL username aur password se isko badlein
-DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/library_db"
+# Load environment variables from .env file
+load_dotenv()
+
+# Get DATABASE_URL from .env
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable not set.")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
