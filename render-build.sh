d7e97dc39a41a94dd588f564dbb1c3d26be127ba
Tarique Ansari <113173007+tarique143@users.noreply.github.com>
2025-07-09 23:21:15 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,6 +3,8 @@
 echo "📦 Installing dependencies from requirements.txt"
 pip install -r library_backend/requirements.txt
 
-echo "🔧 Running Alembic..."
+echo "📁 Moving to backend folder"
 cd library_backend
+
+echo "🗃️ Running alembic..."
 alembic stamp head
