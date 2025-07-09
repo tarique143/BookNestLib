281cf94b4dfc054a8ef319119e8bb3e92e57bdb0
Tarique <tarique272192@gmail.com>
2025-07-09 15:37:25 +0530

    Initial commit of BookNestLib – Library Management System

--- database.py
+++ database.py
@@ -1,3 +1,18 @@
+<<<<<<< HEAD
+=======
+# # file: database.py
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+
+# # Apne MySQL username aur password se isko badlein
+# DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/library_db"
+
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
+
+>>>>>>> 2279b70 (Initial commit of BookNestLib – Library Management System)
 # postgraysql
 # file: database.py
 import os
